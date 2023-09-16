54b7cfeabb37a286f18f91a15cbc2fb09bc90318
Dark R4ger <saravanangowtham15@gmail.com>
2023-09-16 23:43:13 +0530

    modified contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -5,8 +5,8 @@ export default function About() {
   let phrase = "Get in Touch"
   return (
     <Layout title='Contact'>
-      <div className="flex mx-4 flex-col justify-center gap-20 items-center min-h-screen">
-        <h1 className="text-6xl">Get in <span className="text-red-500">Touch</span></h1>
+      <div className=" flex mx-4 flex-col pt-[100px] justify-center gap-20 items-center min-h-screen">
+        <h1 className="text-4xl md:text-6xl">Get in <span className="text-red-500">Touch</span></h1>
         <ContactForm />
       </div>
     </Layout>
