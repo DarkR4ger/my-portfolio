5911dba02ae49137f297b58ca554f5eeedba52ae
DarkR4ger <saravanangowtham15@gmail.com>
2023-11-20 19:45:48 +0530

    modified some stuff

--- src/components/grid-items.js
+++ src/components/grid-items.js
@@ -1,28 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
+import Image from "next/image";
+import Link from "next/link";
 
 export const WorkGridItem = ({ children, id, title, thumbnail }) => {
   return (
-    <section className='w-full text-center group '>
-      <Link
-        href={`/projects/${id}`}
-        scroll={false}
-        className=''
-      >
-        <div className='border group-hover:border-red-500 rounded-xl overflow-hidden transition-all duration-500'>
+    <section className="w-full text-center group ">
+      <Link href={`/projects/${id}`} scroll={false} className="">
+        <div className="border group-hover:border-red-500 rounded-xl overflow-hidden transition-all duration-500">
           <Image
             src={thumbnail}
             alt={title}
-            className='rounded-xl group-hover:scale-110 transition-transform duration-500'
-            placeholder='blur'
+            className="rounded-xl group-hover:scale-110 transition-transform duration-500"
+            placeholder="blur"
           />
         </div>
-        <p className='mt-4 text-xl md:text-4xl text-red-500'>{title}</p>
-        <p className='mt-2 text-xl'>{children}</p>
-
+        <p className="mt-4 text-xl md:text-4xl text-red-500">{title}</p>
+        <p className="mt-2 text-xl">{children}</p>
       </Link>
-
     </section>
-  )
-}
+  );
+};
